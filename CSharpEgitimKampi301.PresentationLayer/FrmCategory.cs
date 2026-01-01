e6bb7f783e17e660f20eb04ed310160022346179
emirhan-coban <emirhancob4n@gmail.com>
2026-01-01 17:59:27 +0300

    20/30

---
+++ CSharpEgitimKampi301.PresentationLayer/FrmCategory.cs
@@ -0,0 +1,39 @@
+﻿using CSharpEgitimKampi301.BusinessLayer.Abstract;
+using CSharpEgitimKampi301.BusinessLayer.Concrete;
+using CSharpEgitimKampi301.DataAccessLayer.EntityFramework;
+using System;
+using System.Windows.Forms;
+
+namespace CSharpEgitimKampi301.PresentationLayer
+{
+    public partial class FrmCategory : Form
+    {
+        private readonly ICategoryService _categoryService;
+        public FrmCategory()
+        {
+            _categoryService = new CategoryManager(new EfCategoryDal());
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            var categoryValues = _categoryService.TGetAll();
+            dataGridView1.DataSource = categoryValues;
+        }
+    }
+}
